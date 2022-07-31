9ce475cbe37f80d7941cda9bf888cf1ea594375c
Jasurbek <jasurisoqov685@gmail.com>
2022-07-31 06:49:57 +0500

    UcHomeVideo components style desctop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Footer from "./Layouts/Footer";
+import UcHomeVideo from "./Components/UcHomeVideo";
+
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// style
 import "./Assets/Styles/index.scss";
 import "./Assets/Styles/footer.scss";
+import "./Assets/Styles/ucHomeVideo.scss";
 
+// image
 import "./Assets/Images/Footer/footer-logo.svg";
 import "./Assets/Fonts/Gilroy/Gilroy-Regular.ttf";
 
 function App() {
   return (
     <>
-      <Footer />
+      <UcHomeVideo />
+      {/* <Footer /> */}
     </>
   );
 }
---
+++ src/Components/UcHomeVideo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ucHomeVideoImg from "../Assets/Images/UcHomeVideo/UcHomeVideo.svg";
+
+const UcHomeVideo = () => {
+  return (
+    <div className="container">
+      <div className="ucHomeVideo">
+        <div className="ucHomeVideo__left">
+          <p className="ucHomeVideo__left--title">Urologic Complex</p>
+          <p className="ucHomeVideo__left--description">
+            Урологическая клиника “Urologic Complex” — современный центр по
+            решению урологических, гинекологических, андрологических и
+            онкологических проблем консервативными и малоинвазивными методами.
+          </p>
+          <div className="ucHomeVideo__left--button btn">Подробнее</div>
+        </div>
+        <div className="ucHomeVideo__right">
+          <img src={ucHomeVideoImg} alt="video" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UcHomeVideo;
