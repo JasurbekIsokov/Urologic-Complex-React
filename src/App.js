08426e1ac95938d59918386fc0182a141a4f847a
Jasurbek <jasurisoqov685@gmail.com>
2022-08-02 12:48:40 +0500

    rabota components style desctop

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Layouts/Footer";
 import UcHomeVideo from "./Components/UcHomeVideo";
 import BlogCliniki from "./Components/BlogCliniki";
+import Rabota from "./Components/Rabota";
 
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ import "./Assets/Styles/index.scss";
 import "./Assets/Styles/footer.scss";
 import "./Assets/Styles/ucHomeVideo.scss";
 import "./Assets/Styles/blogCliniki.scss";
+import "./Assets/Styles/rabota.scss";
 
 // image
 import "./Assets/Images/Footer/footer-logo.svg";
@@ -20,6 +22,7 @@ import "./Assets/Fonts/Gilroy/Gilroy-Regular.ttf";
 function App() {
   return (
     <>
+      <Rabota />
       <UcHomeVideo />
       <BlogCliniki />
       <Footer />
---
+++ src/Components/Rabota.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Rabota = () => {
+  return (
+    <div className="container">
+      <section className="rabota">
+        <p className="rabota__title">Медицинская клиника работает постоянно</p>
+        <p className="rabota__description">
+          Медицинская клиника работает в постоянном контакте с лучшими врачами
+          всего мира Медицинская клиника работает в постоянном контакте
+        </p>
+        <button className="rabota__button btn">Узнать подробнее</button>
+      </section>
+    </div>
+  );
+};
+
+export default Rabota;
