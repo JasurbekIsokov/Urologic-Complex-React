c38dfb86bbfec975500af5326cd5cd2e37cc15c9
Jasurbek <jasurisoqov685@gmail.com>
2022-08-02 18:03:06 +0500

    Our services components style responcive maw-width: 768

--- src/Components/OurServices.jsx
+++ src/Components/OurServices.jsx
@@ -13,120 +13,132 @@ import element from "../Assets/Images/OurServices/element.svg";
 import arrow from "../Assets/Images/OurServices/arrow.svg";
 import btnArrow from "../Assets/Images/OurServices/btn-arrow.svg";
 import whiteArrow from "../Assets/Images/OurServices/white-arrow.svg";
+import mobileElementLeft from "../Assets/Images/OurServices/element-mobile-left.svg";
+import mobileElementRight from "../Assets/Images/OurServices/element-mobile-right.svg";
 
 const OurServices = () => {
   return (
-    <div className="container">
-      <section className="services">
-        <div className="services__boxbox">
-          <p className="services__boxbox--title">Наши услуги</p>
-          <p className="services__boxbox--description">
-            У нас вы найдете все услуги, необходимые для поддержания здоровья
-            вашей семьи. У нас вы найдете все услуги, необходимые для
-            поддержания здоровья вашей семьи
-          </p>
-          <button className="btn">
-            Все услуги
-            <img src={btnArrow} alt="arrow" />
-          </button>
-        </div>
-        <div className="services__box urologiya">
-          <div className="services__box--top">
-            <img src={urologiya} alt="Services" />
-            <p>Урология</p>
+    <>
+      <div className="container">
+        <section className="services">
+          <div className="services__boxbox">
+            <p className="services__boxbox--title">Наши услуги</p>
+            <p className="services__boxbox--description">
+              У нас вы найдете все услуги, необходимые для поддержания здоровья
+              вашей семьи. У нас вы найдете все услуги, необходимые для
+              поддержания здоровья вашей семьи
+            </p>
+            <button className="btn">
+              Все услуги
+              <img src={btnArrow} alt="arrow" />
+            </button>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
+          <div className="services__box urologiya">
+            <div className="services__box--top">
+              <img src={urologiya} alt="Services" />
+              <p>Урология</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-        </div>
 
-        <div className="services__box uroginekologiya">
-          <div className="services__box--top">
-            <img src={uroginekologiya} alt="Services" />
-            <p>Урогинекология</p>
+          <div className="services__box uroginekologiya">
+            <div className="services__box--top">
+              <img src={uroginekologiya} alt="Services" />
+              <p>Урогинекология</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box element1 ">
-          <img src={element1} alt="element" />
-        </div>
+          <div className="services__box element1 ">
+            <img src={element1} alt="element" />
+          </div>
 
-        <div className="services__box onkologiya">
-          <div className="services__box--top">
-            <img src={onkologiya} alt="Services" />
-            <p>Онкология</p>
+          <div className="services__box onkologiya">
+            <div className="services__box--top">
+              <img src={onkologiya} alt="Services" />
+              <p>Онкология</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box andrologiya">
-          <div className="services__box--top">
-            <img src={andrologiya} alt="Services" />
-            <p>Андрология</p>
+          <div className="services__box andrologiya">
+            <div className="services__box--top">
+              <img src={andrologiya} alt="Services" />
+              <p>Андрология</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={whiteArrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={whiteArrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box xirurgiya">
-          <div className="services__box--top">
-            <img src={xirurgiya} alt="Services" />
-            <p>Хирургия</p>
+          <div className="services__box xirurgiya">
+            <div className="services__box--top">
+              <img src={xirurgiya} alt="Services" />
+              <p>Хирургия</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box ginekologiya">
-          <div className="services__box--top">
-            <img src={ginekologiya} alt="Services" />
-            <p>Гинекология</p>
+          <div className="services__box ginekologiya">
+            <div className="services__box--top">
+              <img src={ginekologiya} alt="Services" />
+              <p>Гинекология</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box analiz">
-          <div className="services__box--top">
-            <img src={analiz} alt="Services" />
-            <p>Анализы</p>
+          <div className="services__box analiz">
+            <div className="services__box--top">
+              <img src={analiz} alt="Services" />
+              <p>Анализы</p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box  konsultatsiya">
-          <div className="services__box--top">
-            <img src={konsultatsiya} alt="Services" />
-            <p>Консультация </p>
+          <div className="services__box  konsultatsiya">
+            <div className="services__box--top">
+              <img src={konsultatsiya} alt="Services" />
+              <p>Консультация </p>
+            </div>
+            <div className="services__box--bottom">
+              <p>Уроло́гия — область клинической медицины.</p>
+              <img src={arrow} alt="arrow" />
+            </div>
           </div>
-          <div className="services__box--bottom">
-            <p>Уроло́гия — область клинической медицины.</p>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </div>
 
-        <div className="services__box element">
-          <img src={element} alt="element" />
-        </div>
-      </section>
-    </div>
+          <div className="services__box element">
+            <img src={element} alt="element" />
+          </div>
+        </section>
+      </div>
+      <div className="services__bottom">
+        <img src={mobileElementLeft} alt="element" />
+        <button className="btn">
+          Все услуги
+          <img src={btnArrow} alt="arrow" />
+        </button>
+        <img src={mobileElementRight} alt="element" />
+      </div>
+    </>
   );
 };
 
