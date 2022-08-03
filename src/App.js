4bb4254b370527ddd12df7d78146f88b40082359
Jasurbek <jasurisoqov685@gmail.com>
2022-08-03 16:31:40 +0500

    Header style desctop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./Layouts/Header.jsx";
 import Footer from "./Layouts/Footer";
 import UcHomeVideo from "./Components/UcHomeVideo";
 import BlogCliniki from "./Components/BlogCliniki";
 import Rabota from "./Components/Rabota";
 import Pochemu from "./Components/Pochemu";
 import OurServices from "./Components/OurServices";
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // style
 import "./Assets/Styles/index.scss";
@@ -18,6 +17,9 @@ import "./Assets/Styles/blogCliniki.scss";
 import "./Assets/Styles/rabota.scss";
 import "./Assets/Styles/pochemu.scss";
 import "./Assets/Styles/OurServices.scss";
+import "./Assets/Styles/header.scss";
+
+import "antd/dist/antd.css";
 
 // image
 import "./Assets/Images/Footer/footer-logo.svg";
@@ -26,6 +28,7 @@ import "./Assets/Fonts/Gilroy/Gilroy-Regular.ttf";
 function App() {
   return (
     <>
+      <Header />
       <Rabota />
       <Pochemu />
       <OurServices />
---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+import logo from "../Assets/Images/Header/header-logo.svg";
+import search from "../Assets/Images/Header/search.svg";
+import searchRes from "../Assets/Images/Header/search-res.svg";
+import uzFlag from "../Assets/Images/Header/uz-flag.svg";
+import ruFlag from "../Assets/Images/Header/ru-flag.svg";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="header__left">
+          <img src={logo} alt="logo" />
+          <p style={{ marginBottom: "0" }} className="header__left--logoName">
+            UROLOGIC COMPLEX
+          </p>
+        </div>
+        <navbar className="navbar">
+          <ul style={{ marginBottom: "0" }}>
+            <li>Главная</li>
+            <li>Прайс лист</li>
+
+            <div className="dropdown">
+              <li className="dropbtn">
+                О нас <i className="fa-solid fa-angle-down"></i>
+              </li>
+              <div className="dropdown-content">
+                <li>Описание</li>
+                <li>FAQ</li>
+                <li>Врачи </li>
+                <li> Стационар</li>
+                <li>Отзывы</li>
+              </div>
+            </div>
+
+            <div className="dropdown">
+              <li className="dropbtn">
+                Услуги <i className="fa-solid fa-angle-down"></i>
+              </li>
+              <div className="dropdown-content">
+                <li>Консультация</li>
+                <li>Физиотерапия</li>
+                <li>Хирургия </li>
+                <li> Урология</li>
+                <li>Андрология</li>
+                <li>Анализы</li>
+                <li>УЗД</li>
+                <li>МСКТ</li>
+                <li>Урогинекология</li>
+                <li>Онкология</li>
+                <li>Гинекология</li>
+              </div>
+            </div>
+
+            <li>Статьи</li>
+            <li>Акции</li>
+          </ul>
+        </navbar>
+
+        <div className="header__right">
+          <div className="header__right--search">
+            <img src={search} alt="search icon" />
+          </div>
+          <div className="header__right--language">
+            <ul style={{ marginBottom: "0" }}>
+              <div className="dropdown languageDrop">
+                <li className="dropbtn languageDropbtn">
+                  Рус<i className="fa-solid fa-angle-down"></i>
+                </li>
+                <div className="dropdown-content dropdown-content-language">
+                  <li>
+                    Узб
+                    <img src={uzFlag} alt="uzFlag" />
+                  </li>
+                  <li>
+                    Рус
+                    <img src={ruFlag} alt="ruFlag" />
+                  </li>
+                </div>
+              </div>
+            </ul>
+          </div>
+          <p
+            style={{ marginBottom: "0" }}
+            className="header__right--kontakt btn"
+          >
+            Контакты
+          </p>
+        </div>
+      </header>
+
+      {/* gamburer icon qo'yiladi */}
+
+      <div className="searchInput">
+        <img src={searchRes} alt="search icon" />
+        <input type="text" className="searchRes" placeholder="Введите запрос" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+//  <i className="fa-solid fa-angle-down"></i>;
