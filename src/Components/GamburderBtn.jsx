a1f4613329823979725121df0cff7911c50f3101
Jasurbek <jasurisoqov685@gmail.com>
2022-08-04 07:48:01 +0500

    Mobile menu

--- src/Components/GamburderBtn.jsx
+++ src/Components/GamburderBtn.jsx
@@ -6,6 +6,10 @@ import ruFlag from "../Assets/Images/Header/ru-flag.svg";
 import vector from "../Assets/Images/Header/vector.svg";
 import vector2 from "../Assets/Images/Header/vector2.svg";
 
+import logo from "../Assets/Images/Header/header-logo.svg";
+import search from "../Assets/Images/Header/search.svg";
+import searchRes from "../Assets/Images/Header/search-res.svg";
+
 import arrowMibile from "../Assets/Images/Header/arrow-mobile.svg";
 
 const GamburderBtn = () => {
@@ -23,71 +27,90 @@ const GamburderBtn = () => {
   };
 
   return (
-    <div className="mobile_button">
-      <div onClick={myFunction}>
-        <img src={img ? vector : vector2} alt="vector" />
+    <>
+      <div className="mobile_button">
+        <div onClick={myFunction} className=" ">
+          <img src={img ? vector : vector2} alt="vector" />
+        </div>
       </div>
 
       <div className="mobile" style={{ display: test }}>
-        <hr className="row" />
-        <div className="mobile__language">
-          <p className="mobile__language--title">Язык</p>
-          <div className="mobile__language--box">
-            <div className="mobile__language--box-left">
-              <div className="language_img">
-                <img src={ruFlag} alt="rus" />
-              </div>
-              <p>Русский</p>
+        <div className="container">
+          <header className="header">
+            <div className="header__left">
+              <img src={logo} alt="logo" />
+              <p
+                style={{ marginBottom: "0" }}
+                className="header__left--logoName"
+              >
+                UROLOGIC COMPLEX
+              </p>
             </div>
-            <div className="checkBox checkBox__active">
-              <div className="checkBox-active"></div>
+            <div className="mobile_button">
+              <div onClick={myFunction} className=" ">
+                <img src={img ? vector : vector2} alt="vector" />
+              </div>
+            </div>
+          </header>
+
+          <hr className="row" />
+          <div className="mobile__language">
+            <p className="mobile__language--title">Язык</p>
+            <div className="mobile__language--box">
+              <div className="mobile__language--box-left">
+                <div className="language_img">
+                  <img src={ruFlag} alt="rus" />
+                </div>
+                <p>Русский</p>
+              </div>
+              <div className="checkBox checkBox__active">
+                <div className="checkBox-active"></div>
+              </div>
+            </div>
+            <div className="mobile__language--box">
+              <div className="mobile__language--box-left">
+                <div className="language_img">
+                  <img src={uzFlag} alt="uzb" />
+                </div>
+                <p>Узбекский</p>
+              </div>
+              <div className="checkBox"></div>
             </div>
           </div>
-          <div className="mobile__language--box">
-            <div className="mobile__language--box-left">
-              <div className="language_img">
-                <img src={uzFlag} alt="uzb" />
-              </div>
-              <p>Узбекский</p>
-            </div>
-            <div className="checkBox"></div>
+
+          <div className="mobile__head">
+            <ul>
+              <li>
+                Главная
+                <img src={arrowMibile} />
+              </li>
+              <li>
+                Прайс лист
+                <img src={arrowMibile} />
+              </li>
+              <li>
+                О нас
+                <img src={arrowMibile} />
+              </li>
+              <li>
+                Услуги
+                <img src={arrowMibile} />
+              </li>
+              <li>
+                Статьи
+                <img src={arrowMibile} />
+              </li>
+              <li>
+                Акции
+                <img src={arrowMibile} />
+              </li>
+            </ul>
           </div>
+
+          <p className="mobile__bottom btn">Контакты</p>
         </div>
-
-        <div className="mobile__head">
-          <ul>
-            <li>
-              Главная
-              <img src={arrowMibile} />
-            </li>
-            <li>
-              Прайс лист
-              <img src={arrowMibile} />
-            </li>
-            <li>
-              О нас
-              <img src={arrowMibile} />
-            </li>
-            <li>
-              Услуги
-              <img src={arrowMibile} />
-            </li>
-            <li>
-              Статьи
-              <img src={arrowMibile} />
-            </li>
-            <li>
-              Акции
-              <img src={arrowMibile} />
-            </li>
-          </ul>
-        </div>
-
-        <p className="mobile__bottom btn">Контакты</p>
-
-        <div className="back-color"></div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -10,93 +10,16 @@ import GamburderBtn from "../Components/GamburderBtn";
 
 const Header = () => {
   return (
-    <div className="container">
-      <header className="header">
-        <div className="header__left">
-          <img src={logo} alt="logo" />
-          <p style={{ marginBottom: "0" }} className="header__left--logoName">
-            UROLOGIC COMPLEX
-          </p>
-        </div>
-        <navbar className="navbar">
-          <ul style={{ marginBottom: "0" }}>
-            <li>Главная</li>
-            <li>Прайс лист</li>
-
-            <div className="dropdown">
-              <li className="dropbtn">
-                О нас <i className="fa-solid fa-angle-down"></i>
-              </li>
-              <div className="dropdown-content">
-                <li>Описание</li>
-                <li>FAQ</li>
-                <li>Врачи </li>
-                <li> Стационар</li>
-                <li>Отзывы</li>
-              </div>
-            </div>
-
-            <div className="dropdown">
-              <li className="dropbtn">
-                Услуги <i className="fa-solid fa-angle-down"></i>
-              </li>
-              <div className="dropdown-content">
-                <li>Консультация</li>
-                <li>Физиотерапия</li>
-                <li>Хирургия </li>
-                <li> Урология</li>
-                <li>Андрология</li>
-                <li>Анализы</li>
-                <li>УЗД</li>
-                <li>МСКТ</li>
-                <li>Урогинекология</li>
-                <li>Онкология</li>
-                <li>Гинекология</li>
-              </div>
-            </div>
-
-            <li>Статьи</li>
-            <li>Акции</li>
-          </ul>
-        </navbar>
-
-        <div className="header__right">
-          <div className="header__right--search">
-            <img src={search} alt="search icon" />
-          </div>
-          <div className="header__right--language">
-            <ul style={{ marginBottom: "0" }}>
-              <div className="dropdown languageDrop">
-                <li className="dropbtn languageDropbtn">
-                  Рус<i className="fa-solid fa-angle-down"></i>
-                </li>
-                <div className="dropdown-content dropdown-content-language">
-                  <li>
-                    Узб
-                    <img src={uzFlag} alt="uzFlag" />
-                  </li>
-                  <li>
-                    Рус
-                    <img src={ruFlag} alt="ruFlag" />
-                  </li>
-                </div>
-              </div>
-            </ul>
-          </div>
-          <p
-            style={{ marginBottom: "0" }}
-            className="header__right--kontakt btn"
-          >
-            Контакты
-          </p>
-        </div>
-
-        <GamburderBtn />
-      </header>
-
-      <div className="resHeader">
+    <div className="header-back">
+      <div className="container">
         <header className="header">
-          <navbar className="resNavbar">
+          <div className="header__left">
+            <img src={logo} alt="logo" />
+            <p style={{ marginBottom: "0" }} className="header__left--logoName">
+              UROLOGIC COMPLEX
+            </p>
+          </div>
+          <navbar className="navbar">
             <ul style={{ marginBottom: "0" }}>
               <li>Главная</li>
               <li>Прайс лист</li>
@@ -137,16 +60,97 @@ const Header = () => {
               <li>Акции</li>
             </ul>
           </navbar>
-        </header>
-      </div>
 
-      <div className="searchInput">
-        <img src={searchRes} alt="search icon" />
-        <input type="text" className="searchRes" placeholder="Введите запрос" />
+          <div className="header__right">
+            <div className="header__right--search">
+              <img src={search} alt="search icon" />
+            </div>
+            <div className="header__right--language">
+              <ul style={{ marginBottom: "0" }}>
+                <div className="dropdown languageDrop">
+                  <li className="dropbtn languageDropbtn">
+                    Рус<i className="fa-solid fa-angle-down"></i>
+                  </li>
+                  <div className="dropdown-content dropdown-content-language">
+                    <li>
+                      Узб
+                      <img src={uzFlag} alt="uzFlag" />
+                    </li>
+                    <li>
+                      Рус
+                      <img src={ruFlag} alt="ruFlag" />
+                    </li>
+                  </div>
+                </div>
+              </ul>
+            </div>
+            <p
+              style={{ marginBottom: "0" }}
+              className="header__right--kontakt btn"
+            >
+              Контакты
+            </p>
+          </div>
+          <GamburderBtn />
+        </header>
+
+        <div className="resHeader">
+          <header className="header">
+            <navbar className="resNavbar">
+              <ul style={{ marginBottom: "0" }}>
+                <li>Главная</li>
+                <li>Прайс лист</li>
+
+                <div className="dropdown">
+                  <li className="dropbtn">
+                    О нас <i className="fa-solid fa-angle-down"></i>
+                  </li>
+                  <div className="dropdown-content">
+                    <li>Описание</li>
+                    <li>FAQ</li>
+                    <li>Врачи </li>
+                    <li> Стационар</li>
+                    <li>Отзывы</li>
+                  </div>
+                </div>
+
+                <div className="dropdown">
+                  <li className="dropbtn">
+                    Услуги <i className="fa-solid fa-angle-down"></i>
+                  </li>
+                  <div className="dropdown-content">
+                    <li>Консультация</li>
+                    <li>Физиотерапия</li>
+                    <li>Хирургия </li>
+                    <li> Урология</li>
+                    <li>Андрология</li>
+                    <li>Анализы</li>
+                    <li>УЗД</li>
+                    <li>МСКТ</li>
+                    <li>Урогинекология</li>
+                    <li>Онкология</li>
+                    <li>Гинекология</li>
+                  </div>
+                </div>
+
+                <li>Статьи</li>
+                <li>Акции</li>
+              </ul>
+            </navbar>
+          </header>
+        </div>
+
+        {/* <div className="searchInput">
+          <img src={searchRes} alt="search icon" />
+          <input
+            type="text"
+            className="searchRes"
+            placeholder="Введите запрос"
+          />
+        </div> */}
       </div>
     </div>
   );
 };
 
 export default Header;
-//  <i className="fa-solid fa-angle-down"></i>;
