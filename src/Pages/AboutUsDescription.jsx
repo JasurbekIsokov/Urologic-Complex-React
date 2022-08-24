1a147b2e796fedc860bf14f8a4109fda56126b79
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 12:01:24 +0500

    AboutUsDescription pgae header,footer and form section

--- src/Pages/AboutUsDescription.jsx
+++ src/Pages/AboutUsDescription.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
 
 const AboutUsDescription = () => {
   return (
-    <div>
-      <h1>AboutUsDescription</h1>
+    <div className="aboutUsDesciption-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
-}
+};
 
-export default AboutUsDescription
+export default AboutUsDescription;
