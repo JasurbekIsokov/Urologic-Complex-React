cbd754182687d300aeafb0f40c3ec750ca677152
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 11:55:57 +0500

    OurServices page header, form and footer section

--- src/Pages/OurServices.jsx
+++ src/Pages/OurServices.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
+
 const OurServices = () => {
   return (
-    <div>
-      <h1>OurServices</h1>
+    <div className="ourServices-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
 };
