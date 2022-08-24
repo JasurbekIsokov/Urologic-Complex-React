a3884281fffcab203499a913c961f374069cab97
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 15:27:45 +0500

    stock page

---
+++ src/Components/AksiyBlogCliniki.jsx
@@ -0,0 +1,213 @@
+import React from "react";
+
+import arrow from "../Assets/Images/BlogCliniki/blogCliniki-arrow.svg";
+import timer from "../Assets/Images/BlogCliniki/timer.svg";
+import glaz from "../Assets/Images/BlogCliniki/glaza.svg";
+
+const AksiyBlogCliniki = () => {
+  return (
+    <div className="container">
+      <div className="articlesBlogCliniki__top">
+        <p className="articlesBlogCliniki__top--title">Акции</p>
+      </div>
+      <section className="articlesBlogCliniki">
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box1"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Операция на Варикоцеле в Ташкенте | Лучшие Урологи и Хирурги
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Проведение операции на Варикоцеле в Ташкенте от лучших хирургов в
+            клинике Urologic Complex. Проведение операции на Варикоцеле в
+            Ташкенте от лучших хирургов в клинике Urologic Complex
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box2"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Что такое ПСА? Норма ПСА в крови
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Онкологические заболевания являются бичом нашего времени. После
+            сердечно-сосудистых болезней они занимают совсем непочетное второе
+            место по причинам летального исхода. Особенно сильно подвержены
+            раковым заболеваниям мужчины. Но, благодаря современным методам
+            раннего диагностирования.
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box3"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Травмы полового члена
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Онкологические заболевания являются бичом нашего времени. После
+            сердечно-сосудистых болезней они занимают совсем непочетное второе
+            место по причинам летального исхода. Особенно сильно подвержены
+            раковым заболеваниям мужчины. Но, благодаря современным методам
+            раннего диагностирования.
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box4"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Тестостерон и его значение для мужчины
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Для чего нужен тестостерон, его функция в организме мужчины.
+            Отклонение тестостерона от нормы в одну и другую сторону приводит к
+            негативным последствиям. Влияние «мужского» гормона на активность в
+            постели.
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box5"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Операция на Варикоцеле в Ташкенте | Лучшие Урологи и Хирурги
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Проведение операции на Варикоцеле в Ташкенте от лучших хирургов в
+            клинике Urologic Complex. Проведение операции на Варикоцеле в
+            Ташкенте от лучших хирургов в клинике Urologic Complex
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="articlesBlogCliniki__left">
+          <div className="img-box img-box6"></div>
+          <div className="articlesBlogCliniki__left--time">
+            <p className="articlesBlogCliniki__left--time-year">
+              16 апреля 2022 г.
+            </p>
+            <img src={timer} alt="timer" />
+            <p className="articlesBlogCliniki__left--time-hour">14:00</p>
+          </div>
+          <p className="articlesBlogCliniki__left--title">
+            Что такое ПСА? Норма ПСА в крови
+          </p>
+          <p className="articlesBlogCliniki__left--description">
+            Онкологические заболевания являются бичом нашего времени. После
+            сердечно-сосудистых болезней они занимают совсем непочетное второе
+            место по причинам летального исхода. Особенно сильно подвержены
+            раковым заболеваниям мужчины. Но, благодаря современным методам
+            раннего диагностирования.
+          </p>
+          <div className="articlesBlogCliniki__left--bottom">
+            <div className="articlesBlogCliniki__left--button">
+              <p>Подробнее</p>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="articlesBlogCliniki__left--glaz">
+              <img src={glaz} alt="g" />
+              <p>48</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Pagenation --> */}
+
+      <div class="pagenation">
+        <i class="fa-solid fa-chevron-left"></i>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <i class="fa-solid fa-ellipsis dod"></i>
+        <span>10</span>
+        <i class="fa-solid fa-chevron-right"></i>
+      </div>
+
+      {/* <!-- Pagination --> */}
+    </div>
+  );
+};
+
+export default AksiyBlogCliniki;
--- src/Pages/Stock.jsx
+++ src/Pages/Stock.jsx
@@ -4,11 +4,13 @@ import Header from "../Layouts/Header.jsx";
 import Form from "../Components/Form.jsx";
 import GlobalArrow from "../Components/GlobalArrow.jsx";
 import Footer from "../Layouts/Footer";
+import AksiyBlogCliniki from "../Components/AksiyBlogCliniki.jsx";
 
 const Stock = () => {
   return (
     <div className="stock-page">
       <Header />
+      <AksiyBlogCliniki />
       <Form />
       <GlobalArrow />
       <Footer />
