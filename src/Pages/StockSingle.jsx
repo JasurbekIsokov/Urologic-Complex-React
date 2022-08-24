31802b95ba3febf41338db560956b4f1c1d1976d
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 11:57:45 +0500

    StockSingle page header, form and footer section

--- src/Pages/StockSingle.jsx
+++ src/Pages/StockSingle.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
 
 const StockSingle = () => {
   return (
-    <div>
-      <h1>StockSingle</h1>
+    <div className="stockSingle-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
-}
+};
 
-export default StockSingle
+export default StockSingle;
