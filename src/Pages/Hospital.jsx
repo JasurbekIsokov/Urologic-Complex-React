6878fd84ac7fc8e70778f172945c431969051d01
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 11:55:04 +0500

    Hospital page header, form and footer section

--- src/Pages/Hospital.jsx
+++ src/Pages/Hospital.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
 
 const Hospital = () => {
   return (
-    <div>
-      <h1>Hospital</h1>
+    <div className="hospital-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
-}
+};
 
-export default Hospital
+export default Hospital;
