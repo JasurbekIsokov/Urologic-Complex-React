74f1c13ed8f5588d4f65facf0de1392a026164ee
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 11:54:02 +0500

    Faq page header, form and footer section

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
+
 const Faq = () => {
   return (
-    <div>
-      <h1>Faq</h1>
+    <div className="faq-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
 };
