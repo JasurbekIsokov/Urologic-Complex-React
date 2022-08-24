d2bf9bc426688d99975361039f69825ae6af53c6
Jasurbek <jasurisoqov685@gmail.com>
2022-08-24 11:40:58 +0500

    Articles page header, form and footer section

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Header from "../Layouts/Header.jsx";
+import Form from "../Components/Form.jsx";
+import GlobalArrow from "../Components/GlobalArrow.jsx";
+import Footer from "../Layouts/Footer";
 
 const Articles = () => {
   return (
-    <div>
-      <h1>Articles</h1>
+    <div className="articles-page">
+      <Header />
+      <Form />
+      <GlobalArrow />
+      <Footer />
     </div>
   );
-}
+};
 
-export default Articles
+export default Articles;
